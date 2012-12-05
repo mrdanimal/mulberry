27fafecf4941b56a1535ceb7dfc2ecfef2f596c2
Dan Imal <dimal@toura.com>
2012-12-05 11:16:22 +0000

    add comment

--- app/toura/pages/FeedPage.js
+++ app/toura/pages/FeedPage.js
@@ -31,6 +31,11 @@ dojo.declare('toura.pages.FeedPage', mulberry.containers.Page, {
 
   postMixInProperties : function() {
     this.inherited(arguments);
+
+    // Create a baseObj here from the feed (or whatever) with the same
+    // structure as a Node. Do any event setup that would normally be handled
+    // in a Capability here.
+    //
     console.log('this.baseObj.feeds[0]', this.baseObj.feeds[0]);
   }
 });
